90169f4d3eed448189b3ded73f6968b2d31b9bdc
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-01-02 00:41:22 +0100

    added printing data lesson

---
+++ Topics/Printing data/Digits from 0 to 9 backwards/src/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+
+        final int endNumber = 9;
+        final int startNumber = 0;
+
+        for (int i = endNumber; i > startNumber; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.print(startNumber);
+    }
+}
