3e1082f0a0cb3e502192c92be3e8136ad6dec02f
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-01-12 01:21:50 +0100

    added errors in programs lesson

--- Topics/Conditional statement/Queens/src/Main.java
+++ Topics/Conditional statement/Queens/src/Main.java
@@ -4,5 +4,17 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // start coding here
+
+        double x1 = scanner.nextInt();
+        double y1 = scanner.nextInt();
+        double x2 = scanner.nextInt();
+        double y2 = scanner.nextInt();
+
+        if (x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
     }
 }
