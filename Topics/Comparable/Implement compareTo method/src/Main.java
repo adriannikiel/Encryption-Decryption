eefdddffee2878725a0ba149d3e1441713c6f984
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-01-23 23:53:20 +0100

    added Comparable lesson

---
+++ Topics/Comparable/Implement compareTo method/src/Main.java
@@ -0,0 +1,40 @@
+class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Person(String name, int age, int height, int weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public int compareTo(Person otherPerson) {
+        // add your code here!
+        if (name.equals(otherPerson.name)) {
+            return Integer.compare(age, otherPerson.age);
+        } else {
+            return name.compareTo(otherPerson.name);
+        }
+    }
+
+}
