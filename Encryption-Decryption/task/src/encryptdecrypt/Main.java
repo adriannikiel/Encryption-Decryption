baceeda4c23be9a3ad00c7f6a485fab23f15dcaf
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-01-27 02:32:19 +0100

    started 4rd stage of project

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -6,40 +6,37 @@ public class Main {
     public static void main(String[] args) {
 
         final Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
+
+        final String target = scanner.nextLine();
+        final String text = scanner.nextLine();
         final int key = scanner.nextInt();
 
-        System.out.println(encryptText(message, key));
+        switch (target) {
+            case "enc":
+                System.out.println(encryptText(text, key));
+                break;
+            case "dec":
+                System.out.println(decryptText(text, key));
+                break;
+        }
 
     }
 
     public static String encryptText(String message, int key) {
 
-        final char min = 'a';  //97
-        final char max = 'z';  //122
-
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < message.length(); i++) {
 
-            char token = message.charAt(i);
-
-            if (token >= min && token <= max) {
-                char c = (char) (token + key);
-
-                if (c > max) {
-                    c = (char) (min + token + key - max - 1);
-                }
-
-                result.append(c);
-
-            } else {
-                result.append(token);
-            }
+            char c = (char) (message.charAt(i) + key);
+            result.append(c);
         }
 
         return result.toString();
     }
 
+    public static String decryptText(String cyphertext, int key) {
+        return encryptText(cyphertext, -key);
+    }
 
 }
