d2e861a3ec4d54d4c949f0dd09bdcab0630fe3a8
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-01-31 01:05:57 +0100

    added revisions lesson

--- Topics/Integer types and operations/How many nuts each squirrel will get/src/Main.java
+++ Topics/Integer types and operations/How many nuts each squirrel will get/src/Main.java
@@ -4,5 +4,10 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+
+        System.out.println(K / N);
     }
 }
