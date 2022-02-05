b027b831619a3b24a538a60154c9d74f2d3112b1
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-02-05 17:42:12 +0100

    added revisions lesson

---
+++ Topics/String/Replacing 'a' with 'b'/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+
+        String text = scanner.nextLine();
+
+        System.out.println(text.replace('a','b'));
+    }
+}
