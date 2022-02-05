4f71b79406145bb9badb886985bab9d3161e0fc3
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-02-05 14:55:11 +0100

    started 6th stage of project

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,21 +1,90 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String mode = findParam(args, "-mode");
-        String data = findParam(args, "-data");
-        int key = Integer.parseInt(findParam(args, "-key"));
+        String mode = setFinalMode(args);
+        int key = setFinalKey(args);
+        String data = setFinalInput(args);
+        PrintStream stream = setFinalOutput(args);
 
         switch (mode) {
             case "enc":
-                System.out.println(encryptText(data, key));
+                stream.println(encryptText(data, key));
                 break;
             case "dec":
-                System.out.println(decryptText(data, key));
+                stream.println(decryptText(data, key));
                 break;
         }
 
+        if (stream != null) {
+            stream.close();
+        }
+
+    }
+
+    private static String setFinalMode(String[] args) {
+        String mode = findParam(args, "-mode");
+
+        return mode.isEmpty() ? "enc" : mode;
+    }
+
+    private static int setFinalKey(String[] args) {
+        String key = findParam(args, "-key");
+
+        return key.isEmpty() ? 0 : Integer.parseInt(key);
+    }
+
+    private static String setFinalInput(String[] args) {
+
+        String data = findParam(args, "-data");
+        String inFilePath = findParam(args, "-in");
+
+        if (data.isEmpty()) {
+            if (!inFilePath.isEmpty()) {
+
+                File file = new File(inFilePath);
+
+                try (Scanner scanner = new Scanner(file)) {
+                    data = scanner.nextLine();
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error: file " + file.getName() + " doesn't exist!");
+                    System.exit(-1);
+                }
+
+            }
+        }
+
+        return data.isEmpty() ? "" : data;
+
+    }
+
+    private static PrintStream setFinalOutput(String[] args) {
+
+        String outFilePath = findParam(args, "-out");
+        PrintStream printStream = null;
+
+        if (!outFilePath.isEmpty()) {
+
+            File file = new File(outFilePath);
+
+            try {
+                printStream = new PrintStream(file);
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: file " + file.getName() + " doesn't exist!");
+                System.exit(-1);
+            }
+
+        } else {
+            printStream = new PrintStream(System.out);
+        }
+
+        return printStream;
     }
 
     private static String findParam(String[] args, String param) {
